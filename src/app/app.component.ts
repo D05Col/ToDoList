898569a22a569613c865bfeb5446efbda2ea1020
Dylan Collins <djdylan7705@gmail.com>
2025-03-16 10:25:14 +0000

    I know the service is injected, I just dont think the alert is alerting.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { DatePickerComponent } from './date-picker/date-picker.component';
 import { TitleInputComponent } from './title-input/title-input.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { FormsModule } from '@angular/forms';
+import { TaskServiceService } from './services/task-service.service';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, DatePickerComponent, TitleInputComponent, TaskListComponent, FormsModule],
+  providers: [TaskServiceService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
