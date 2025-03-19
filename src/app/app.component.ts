8f2120739977038bddeaf9dc918ea1d1680d483f
Dylan Collins <djdylan7705@gmail.com>
2025-03-19 21:40:08 +0000

    date picker working. Beginning styles. Needs local storage and scoped tasks by day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DatePickerComponent } from './date-picker/date-picker.component';
-import { TitleInputComponent } from './title-input/title-input.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TaskServiceService } from './services/task-service.service';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TitleInputComponent } from './title-input/title-input.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePickerComponent, TitleInputComponent, TaskListComponent, FormsModule, MatDatepickerModule, MatInputModule, MatNativeDateModule, FormsModule, ReactiveFormsModule],
+  imports: [DatePickerComponent, TaskListComponent, TitleInputComponent],
   providers: [TaskServiceService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { MatDatepicker, MatDatepickerToggle } from '@angular/material/datepicker';
-import { MatHint, MatInput } from '@angular/material/input';
+import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker'
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { MatHint } from '@angular/material/form-field';
 @Component({
   selector: 'app-date-picker',
-  imports: [MatDatepicker, MatInput, MatHint, MatDatepickerToggle],
+  imports: [MatDatepickerModule, ReactiveFormsModule, MatNativeDateModule, MatDatepicker, MatHint, FormsModule],
+  providers: [MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.sass'
 })
