17575f83dba7ab896c54b1a6fdbbe3591643a58e
Dylan Collins <djdylan7705@gmail.com>
2025-03-24 21:49:38 +0000

    removed local storage as doesnt work using ssr, add a couple styles

--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -6,33 +6,23 @@ import { ToDoTask } from '../models/ToDoTask';
 })
 export class TaskServiceService {
 
-  public tasks: WritableSignal<ToDoTask[]>;
-  private count: number;
+  public tasks: WritableSignal<ToDoTask[]> = signal([]);
+  count = 0;
 
   Add(title: string): void{
     this.tasks.update(list => {
       const task = new ToDoTask(title, ++this.count);
       return [...list, task];
     });
-    localStorage.setItem("taskList", JSON.stringify(this.tasks));
-    localStorage.setItem("count", JSON.stringify(this.count));
   } 
 
   Remove(id: number): void {
     this.tasks.update(list => {
-      return list.filter(item => item.id != id);
-    });
-    localStorage.setItem("taskList", JSON.stringify(this.tasks));
-    localStorage.setItem("count", JSON.stringify(this.count));
+      return list.filter(item => item.id != id)
+    })
   }
 
-  Complete(id: number): void {;
+  Complete(id: number): void {
     this.tasks().find(item => item.id == id)
-    localStorage.setItem("taskList", JSON.stringify(this.tasks));
-    localStorage.setItem("count", JSON.stringify(this.count));
-  }
-  constructor(){
-    this.tasks = signal(JSON.parse(localStorage.getItem("taskList") ?? "[]"));
-    this.count = JSON.parse(localStorage.getItem("count") ?? "0");
   }
 }
