9f644306db3a38c7397c4c808592be614c3e3ceb
Dylan Collins <djdylan7705@gmail.com>
2025-04-13 10:07:21 +0100

    use angular materials to implement tabbing for completed and active tasks

--- to-do-list/src/app/task-list/task-list.component.ts
+++ to-do-list/src/app/task-list/task-list.component.ts
@@ -2,9 +2,10 @@ import { Component, inject } from '@angular/core';
 import { TaskServiceService } from '../services/task-service.service';
 import {NgFor} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-task-list',
-  imports: [NgFor, MatButtonModule],
+  imports: [NgFor, MatButtonModule, MatTabsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
