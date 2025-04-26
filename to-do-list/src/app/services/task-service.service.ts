8eee8db3ac581635e462bae01f29e83d7309240a
Dylan Collins <djdylan7705@gmail.com>
2025-04-26 19:43:02 +0100

    using complete and active tasks in seperate lists

--- to-do-list/src/app/services/task-service.service.ts
+++ to-do-list/src/app/services/task-service.service.ts
@@ -5,37 +5,45 @@ import { ToDoTask } from '../models/ToDoTask';
   providedIn: 'root'
 })
 export class TaskServiceService {
-  public tasks: WritableSignal<ToDoTask[]> = signal(JSON.parse(localStorage.getItem("tasks") ?? "[]"));
+  public activeTasks: WritableSignal<ToDoTask[]> = signal(JSON.parse(localStorage.getItem("activeTasks") ?? "[]"));
+  public completedTasks: WritableSignal<ToDoTask[]> = signal(JSON.parse(localStorage.getItem("completedTasks") ?? "[]"));
   public date: WritableSignal<Date> = signal(JSON.parse(localStorage.getItem("date") ?? JSON.stringify(new Date())));
   
   count = 0;
 
   Add(title: string): void{
-    this.tasks.update(list => {
+    this.activeTasks.update(list => {
       const task = new ToDoTask(title, ++this.count, this.date());
       return [...list, task];
     });
     
-    localStorage.setItem("tasks", JSON.stringify(this.tasks()));
+    localStorage.setItem("activeTasks", JSON.stringify(this.activeTasks()));
   } 
 
   Remove(id: number): void {
-    this.tasks.update(list => {
+    this.activeTasks.update(list => {
       return list.filter(item => item.id != id);
-    })
-    localStorage.setItem("tasks", JSON.stringify(this.tasks()));
+    });
+    this.completedTasks.update(list => {
+      return list.filter(item => item.id != id);
+    });
+    localStorage.setItem("activeTasks", JSON.stringify(this.activeTasks()));
+    localStorage.setItem("completedTasks", JSON.stringify(this.completedTasks()));
   }
 
   Complete(id: number): void {
-    let item = this.tasks().find(item => item.id == id);
+    let item = this.activeTasks().find(item => item.id == id);
     if (item != null){
       item.completed = true;
+      this.completedTasks.update(list => [...list, item]);
+      this.activeTasks.update(list => list.filter(item => item.id != id));
     }
     else{
       console.log(`Could not find item with Id ${id} to complete`);
     }
     
-    localStorage.setItem("tasks", JSON.stringify(this.tasks()));
+    localStorage.setItem("activeTasks", JSON.stringify(this.activeTasks()));
+    localStorage.setItem("completedTasks", JSON.stringify(this.completedTasks()));
   }
 
   constructor(){
